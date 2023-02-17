1353e7c78d40fabf86d10f0b1db89eacdc5cc896
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-02-17 14:14:42 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,10 +10,10 @@ int main(void)
 	int i = 0;
 
 	while (i < 10)
-    {
+    	{
         	putchar(i + '0');
         	putchar('\n');
         	i++;
-    }
-    return (0);
+    	}
+    	return (0);
 }
