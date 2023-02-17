3804137aec484cb95f9236d95ea582d6cc9ec319
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-02-17 03:10:02 +0300

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ * Description: Prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0. Program execution is a success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
