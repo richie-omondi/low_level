feca52c7a99d4af9f4a49605073d834fc8aa8ac9
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-02-17 03:42:50 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 
 /**
 * main - Entry point for program execution
-* Description: Print a string without using printf or puts followed by a new line to the standard error
+* Description: Print a string without using printf or puts
 * Return: 0. Program execution is a success
 */
 
 int main(void)
 {
-    char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, msg, 51);
-    return (1);
+char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, msg, 59);
+return (1);
 }
