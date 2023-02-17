6e09e18bbd471f968fe4a1305aa0fd919e704fd8
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-02-17 03:13:09 +0300

    Create 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for program execution
+ * Description: use printf to print a string
+ * Return: 0. Program execution is a success
+ */
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
