77837d812960ba8f6aad6ae780ae901d3939698a
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-02-17 03:20:01 +0300

    Create 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for program execution
+ * Description: Prints the size of different data types based on the OS
+ * Return: 0. Program execution is a success.
+ */
+int main(void)
+{
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
+return (0);
+}
