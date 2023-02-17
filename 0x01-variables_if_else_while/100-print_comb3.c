6e9ef73d574c2a2f4249ddd86d37a27cb014a510
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-02-17 14:47:32 +0300

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entyr point for program execution
+ * Description: Prints all possible different combinations of two digits
+ * The two digits must be different
+ * In ascending order
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c, i;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			if (c < i)
+			{
+				putchar(c);
+				putchar(i);
+
+				if (c != '8' || (c == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
