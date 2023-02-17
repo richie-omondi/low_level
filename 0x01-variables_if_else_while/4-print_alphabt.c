959af7d1a36148f77fb9396ec4b58a7cfa906712
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-02-17 13:51:20 +0300

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point for program execution
+ * Description: Prints lowercase alphabet letters except q and e
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		else
+		{
+		}
+	}
+	putchar('\n');
+	return (0);
+}
