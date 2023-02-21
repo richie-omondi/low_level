8f4206b7484cf5bc5e60eb6ef302e0f87d521860
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-02-22 01:18:24 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _islower: checks is a character is lowercase
+ * _islower: Entry point
+ * Description: checks if a character is lowercase
  * @c: the integer value it receives
  * Return: 1 if true. 0 if false.
  */
