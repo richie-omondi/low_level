574e83b77c87f5d5654a3e4094a47c9bdbbf8b38
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-02-24 02:39:42 +0300

    Create 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_square - check for size of square
+ * @n: number of # to be printed
+ * Return: void
+ */
+
+void print_square(int n)
+{
+	int a = 0, b;
+
+	while (a < n && n > 0)
+	{
+		b = 0;
+		while (b < n)
+		{
+			_putchar('#');
+			b++;
+		}
+		_putchar('\n');
+		a++;
+	}
+	if (a == 0)
+		_putchar('\n');
+}
