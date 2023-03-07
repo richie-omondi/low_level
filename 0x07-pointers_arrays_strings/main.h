f4bcbcee1dd4cf5720a6062f7bb7f9d8cb8d236e
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-03-07 03:57:33 +0300

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,7 +11,7 @@
 #include <stddef.h>
 
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
+char *_memset(char *s, char b, unsigned int n);
 void *_memcpy(void *dest, const void *src, size_t n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
