c4d89900f30eb366964a9d7b4e7044e3eac048e5
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-03-11 02:50:43 +0300

    Create

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if a character is digit
+ * @c: integer cariable
+ * Return: 1 if true, 0 if false
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
